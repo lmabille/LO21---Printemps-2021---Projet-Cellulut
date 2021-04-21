03e6dcf2bae166cff06c58ec34da9dd822e3f247
lmabille <mabille.l@icloud.com>
2021-04-21 15:48:14 +0200

    create simulateur class as a singleton

--- fonction.cpp
+++ fonction.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+#include "fonction.h"
+
+Simulateur &Simulateur::donneInstance()
+{
+    if (uniqueInstance == nullptr)
+        uniqueInstance = new Simulateur();
+    return *uniqueInstance;
+}
+
+void Simulateur::libereInstance()
+{
+    delete uniqueInstance;
+    uniqueInstance = nullptr;
+}
---
+++ fonction.h
@@ -0,0 +1,19 @@
+
+class Simulateur
+{
+private:
+    int buffer_config;
+    int clock;
+    static Simulateur *instance_unique;
+    Simulateur();
+    Simulateur(const Simulateur &) = delete;
+    void operator=(const Simulateur &) = delete;
+    static Simulateur *uniqueInstance;
+
+public:
+    static Simulateur &donneInstance();
+    static void libereInstance();
+    void affichage();
+    void reset();
+    void next();
+};
