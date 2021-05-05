3001f6da1be31bc6d0effae3bf3548e9de8d13a5
Orhane Lahneche <orhane.lahneche@etu.utc.fr>
2021-05-05 20:32:40 +0200

    création classe cellule

--- fonction.h
+++ fonction.h
@@ -4,5 +4,19 @@
 
 #ifndef LO21_PRINTEMPS_2021_PROJET_CELLULUT_FONCTION_H
 #define LO21_PRINTEMPS_2021_PROJET_CELLULUT_FONCTION_H
+class cellule {
+    int x;
+    int y;
+    Etat * etat;
+    friend configuration;
+    Cellule(int x, int y) : x(x), y(y) {};
+    void set_etatcellule(Etat* etat);
 
+public:
+
+    void get_x();
+    void get_y();
+    Etat & get_Etat();
+
+};
 #endif //LO21_PRINTEMPS_2021_PROJET_CELLULUT_FONCTION_H
