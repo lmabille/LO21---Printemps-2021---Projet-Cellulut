af6688c8e0410ddceceeed1b027944aed91ddd6b
Laurine Hamard <laurine.hamard@gmail.com>
2021-05-19 12:09:03 +0200

    Modification sur la fonction de transition

--- proj.cpp
+++ proj.cpp
@@ -4,19 +4,3 @@
 bool operator!=(const Etat& e1, const Etat& e2){
     return e1.getLabel()!=e2.getLabel();
 }
-
-
-bool Modele::verificationEtat(){
-    bool same=1;
-    int i;
-    Etat ** const e=fonction->getEtat();
-    while(e[i]!=nullptr && ensembleEtat[i]!=nullptr){
-        if (*e[i]!=*ensembleEtat[i])same=0;
-    }
-    if(e[i]!=nullptr && ensembleEtat[i]==nullptr)same=0;
-    if(e[i]==nullptr && ensembleEtat[i]!=nullptr)same=0;
-
-    return same;
-
-
-}
--- proj.h
+++ proj.h
@@ -1,8 +1,9 @@
 #include<string>
+#include<cstdio>
 
 using namespace std;
 
-class Voisinnage{
+class Voisinage{
 
 };
 
@@ -17,57 +18,35 @@ public :
 
 bool operator!=(const Etat& e1, const Etat& e2);
 
-class Fonction_Transition{ //classe abstraite
-protected :
-    int id;
-    Etat **etatAutorise;
-public :
-    int getId() const {return id; }
-    Etat ** const getEtat() const {return etatAutorise;} // assez sécurisé
-
-};
 
 class Modele{
     string titre; //key
     Etat **ensembleEtat;
-    Fonction_Transition *fonction;
+    string regles[1000];
     Voisinnage *typeVoisinnage;
     string description;
     string auteur;
     unsigned int anneeCreation;
 
 public :
-    Modele(string t, Etat **e, Fonction_Transition *f, Voisinnage *v, string d, string a="",unsigned int annee=0):titre(t), ensembleEtat(e), fonction(f), typeVoisinnage(v), description(d), auteur(a), anneeCreation(annee){}
+    Modele(string t, Etat **e, string r[1000], Voisinnage *v, string d, string a="",unsigned int annee=0):titre(t), ensembleEtat(e), regles(r), typeVoisinnage(v), description(d), auteur(a), anneeCreation(annee){}
     ~Modele(){
         delete[] ensembleEtat;
     }
-    bool verificationEtat();
     const string& getTitre() const {return titre; }
     Etat ** const getEtat() const {return ensembleEtat;}
-    const Fonction_Transition *getFonction() const {return fonction; }
+     const string& getRegles() const {return regles; }
     const Voisinnage *getVoisin() const {return typeVoisinnage; }
     const string& getDescription() const {return description; }
     const string& getAuteur() const {return auteur; }
     int getAnnee() const {return anneeCreation; }
-
-
-
-
-
-
-
-};
-
-
-class TypeFonction : Fonction_Transition{ //sous_classe regroupant les grandes familles de fonctions en fct de leur règles de transitions
-public :
-    TypeFonction(int id, Etat **e);//à implémenter plus tard
-    ~TypeFonction(){
-        delete[] etatAutorise;
-    }
-    appliquerTransition(const Etat& dep, const Etat& arr); //à implémenter plus tard
+    void chargerModele(FILE *f);//charge les éléments du fichier puis fait appel au constructeur
+    void creerModele();//propose choix à l'utilisateur, génère règles puis fait appel au constructeur
+    FILE *sauvegarderModele();//sauvegarde l'ensemble des attributs dans un fichier xml
 
 
 };
+
+
 
 
