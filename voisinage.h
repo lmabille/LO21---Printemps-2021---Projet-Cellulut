d4247c1f470e506bffc787356dbbb9edd3b08064
lmabille <mabille.l@icloud.com>
2021-05-28 14:02:35 +0200

    modify constructor voisinage

--- voisinage.h
+++ voisinage.h
@@ -5,44 +5,40 @@
 #ifndef LO21_PRINTEMPS_2021_PROJET_CELLULUT_VOISINAGE_H
 #define LO21_PRINTEMPS_2021_PROJET_CELLULUT_VOISINAGE_H
 
-using namespace  std;
+using namespace std;
 #include <iostream>
 #include <cstdio>
 
-
-class Case;
-
-class Voisinage {
+class Voisinage
+{
     int nbCelluleVoisi;
     string typeVoisi;
-    Case*  ensemble_case;
+    Case *ensemble_case;
     friend class Modele;
     // friend class Simulateur;
 public:
-    Voisinage();
+    Voisinage(int n) : nbCelluleVoisi(n), ensemble_case(new Case[n]) { typeVoisi = ""; };
     const string getTypeVoisi();
     const int getNbCelluleVoisi() const;
     void setNbCellule(int nb);
-    void setensemble_case(Case * c){ensemble_case=c;};
-    Case * getTableau();
-    Case& operator[](int indice);
+    void setensemble_case(Case *c) { ensemble_case = c; };
+    Case *getTableau();
+    Case &operator[](int indice);
 };
 
-class Case{ // Classe case
+class Case
+{ // Classe case
     int l;
     int c;
     //friend Voisinage;
 
 public:
-    int getL() const {return l;};
-    int getC() const {return c;};
-    void setL(int newL) {this->l = newL;}
-    void setC(int newC) {this->c = newC;}
+    int getL() const { return l; };
+    int getC() const { return c; };
+    void setL(int newL) { this->l = newL; }
+    void setC(int newC) { this->c = newC; }
     Case() = default;
     Case(int L, int C) : l(L), c(C){};
 };
-
-
-
 
 #endif //LO21_PRINTEMPS_2021_PROJET_CELLULUT_VOISINAGE_H
