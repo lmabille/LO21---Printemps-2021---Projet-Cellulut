2e6713e791a9c7febf11dd1612308876c56ff9c0
Orhane Lahneche <orhane.lahneche@etu.utc.fr>
2021-05-30 15:46:19 +0200

    correction bug voisinage

--- voisinage.h
+++ voisinage.h
@@ -9,24 +9,6 @@ using namespace std;
 #include <iostream>
 #include <cstdio>
 
-class Voisinage
-{
-    int nbCelluleVoisi;
-    string typeVoisi;
-    Case *ensemble_case;
-    friend class Modele;
-    // friend class Simulateur;
-public:
-    Voisinage(int n) : nbCelluleVoisi(n), ensemble_case(new Case[n]) { typeVoisi = ""; };
-    const string getTypeVoisi();
-    const int getNbCelluleVoisi() const;
-    void setNbCellule(int nb);
-    void setensemble_case(Case *c) { ensemble_case = c; };
-    void setType(string t) { typeVoisi = t; }
-    Case *getTableau();
-    Case &operator[](int indice);
-};
-
 class Case
 { // Classe case
     int l;
@@ -40,6 +22,25 @@ public:
     void setC(int newC) { this->c = newC; }
     Case() = default;
     Case(int L, int C) : l(L), c(C){};
+};
+
+class Voisinage
+{
+    int nbCelluleVoisi;
+    string typeVoisi;
+    Case *ensemble_case;
+    friend class Modele;
+    // friend class Simulateur;
+public:
+    Voisinage()=default;
+    Voisinage(int n) : nbCelluleVoisi(n), ensemble_case(new Case[n]) { typeVoisi = ""; };
+    const string getTypeVoisi();
+    const int getNbCelluleVoisi() const;
+    void setNbCellule(int nb);
+    void setensemble_case(Case *c) { ensemble_case = c; };
+    void setType(string t) { typeVoisi = t; }
+    Case *getTableau();
+    Case &operator[](int indice);
 };
 
 #endif //LO21_PRINTEMPS_2021_PROJET_CELLULUT_VOISINAGE_H
