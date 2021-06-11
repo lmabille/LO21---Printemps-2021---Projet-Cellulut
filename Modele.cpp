b0e537ff5190e2a137a37b1f7c5d8b1dec08cf9a
lmabille <mabille.l@icloud.com>
2021-06-11 13:26:43 +0200

    ajout commentaire gen regle WireWorld

--- Modele.cpp
+++ Modele.cpp
@@ -93,7 +93,7 @@ string *generation_regles_Griffeath(const unsigned int nb_voisins)
     //Rappel des règles :
     // une cellule passe de l’état i à i + 1 (modulo 4) dès que i + 1 (modulo 4) est présent dans au moins 3 cellules voisines.
 
-    //etat_depart | 0 (jaune) |  1 (orange clair) | 2 (orange foncé) |  3 (rouge) | etat_arrivee
+    //etat_depart | 0 (jaune, i) |  1 (orange clair, j) | 2 (orange foncé, k) |  3 (rouge, l) | etat_arrivee
 
     //EXEMPLE :
     //  013401 --> passe de l'etat 0 à 1 car il y a 3 voisins dans l'état 1 (8 voisins)
@@ -104,13 +104,13 @@ string *generation_regles_Griffeath(const unsigned int nb_voisins)
 
     string tab[500];
     int m = 0;
-    for (size_t i = 0; i < 10; i++) // 0 (jaune)
+    for (size_t i = 0; i < 10; i++) // nb_voisins à 0 (jaune)
     {
-        for (size_t j = 0; j < 10; j++) // 1 (orange clair)
+        for (size_t j = 0; j < 10; j++) // nb_voisins à 1 (orange clair)
         {
-            for (size_t k = 0; k < 10; k++) //  2 (orange foncé)
+            for (size_t k = 0; k < 10; k++) //  nb_voiins à 2 (orange foncé)
             {
-                for (size_t l = 0; l < 10; l++) // 3 (rouge)
+                for (size_t l = 0; l < 10; l++) // nb_voisins à 3 (rouge)
                 {
                     if ((i + j + k + l) == nb_voisins) // Si la somme du quadruplet donne le nombre de voisins
                     {
@@ -144,6 +144,14 @@ string *generation_regles_Griffeath(const unsigned int nb_voisins)
 
 string *generation_regles_Wireworld(const unsigned int nb_voisins)
 {
+    //Rappel règles:
+
+    // Une tête d'électron devient une queue d'électron (R1)
+    // Une queue d'électron devient un conducteur (R2)
+    // Un conducteur devient une tête d'électron si une ou deux des cellules voisines sont des têtes d'électron (R3)
+
+    // etat_depart | nb_voisins_vide(i) |  nb_voisins_conducteur(j) | nb_voisins_electron_tete(k) |  nb_voisins_electron_queue(l) | etat_arrivee
+
     //0 = Vide
     //1 = conducteur
     //2 = tete electron
@@ -151,23 +159,23 @@ string *generation_regles_Wireworld(const unsigned int nb_voisins)
 
     string tab[500];
     int m = 0;
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < 10; i++) //nb_voisins vide
     {
-        for (size_t j = 0; j < 10; j++)
+        for (size_t j = 0; j < 10; j++) //nb_voisins conducteur
         {
-            for (size_t k = 0; k < 10; k++)
+            for (size_t k = 0; k < 10; k++) //nb_voisins  electron tete
             {
-                for (size_t l = 0; l < 10; l++)
+                for (size_t l = 0; l < 10; l++) // nb_voisins electron queue
                 {
-                    if ((i + j + k + l) == nb_voisins)
+                    if ((i + j + k + l) == nb_voisins) //Si la somme du quadruplet donne le nombre de voisins
                     {
-                        tab[m] = "2" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "3";
+                        tab[m] = "2" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "3"; //Ajout de R1 (pas de condition sur voisinage)
                         m++;
-                        tab[m] = "3" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "1";
+                        tab[m] = "3" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "1"; //Ajout de R2 (pas de condition sur voisinage)
                         m++;
-                        if ((k == 1) || (k == 2))
+                        if ((k == 1) || (k == 2)) // si nb_voisins electron tête est de 1 ou 2
                         {
-                            tab[m] = "1" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "2";
+                            tab[m] = "1" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "2"; // alors passage de l'état conducteur à tête electron
                             m++;
                         }
                     }
