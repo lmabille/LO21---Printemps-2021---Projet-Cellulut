b6a6660e67bd970e2e0bc742c8109d617aaf88b0
lmabille <mabille.l@icloud.com>
2021-06-11 12:10:21 +0200

    add generation regle griffeath

--- Modele.cpp
+++ Modele.cpp
@@ -73,114 +73,157 @@ string *generation_regles_Life_game(const unsigned int nb_voisins)
     }
     return tab;
 }
-    // ça arrive
-    char FonctionTransition::comparaison_voisinnage(string voisins, string * trans, char cel, unsigned int limit, int nb_Etat)
+
+string *generation_regles_Griffeath(const unsigned int nb_voisins)
+{
+
+    string tab[500];
+    int m = 0;
+    for (size_t i = 0; i < 10; i++)
     {
-        // pour un voisinnage donné on va vérifier si ce voisinnage est dans
-        // le tableau de fonction transition si oui on va renvoyer un caractère qui
-        // va correspondre à l'état de la cellule à la génération t+1
-        unsigned int i = 0;
-        int test = 0;
-        string st;
-        //cout << "compa_vois"
-        //     << "\n";
-        while (i < limit && (trans[i][0] == cel))
+        for (size_t j = 0; j < 10; j++)
         {
-            //cout << "passeboucle"
-            //     << "\n\n";
-            st = trans[i].substr(1, trans[i].length() - 2);
-            st.append(st);
-            test = st.find(voisins);
-            if (test != -1)
+            for (size_t k = 0; k < 10; k++)
             {
-                size_t j = (trans[i].length() - 1);
-                return trans[i][j];
-            }
-            i++;
-        }
-        return cel;
-    }
-
-    char FonctionTransitionIntention::comparaison_voisinnage(string voisins, string * trans, char cel, unsigned int limit, int nb_Etat)
-    {
-        auto tab = new int[nb_Etat]; //initialisation du tableau
-        for (int i = 0; i < nb_Etat; i++)
-        {
-            tab[i] = 0;
-        }
-
-        for (int i = 0; i < voisins.length(); i++)
-        {
-            tab[voisins[i] - '0']++; //pour chaque cellule du voisinnage on va ajouter une occurence à la case du tableau correspond à son état
-        }
-
-        string tab_de_inten;
-        char c;
-        for (int i = 0; i < nb_Etat; i++)
-        { //ici on crée la chaîne de caractère que l'on va comparer avec les règles de transition
-            c = tab[i] + '0';
-            //cout<<tab[i]<<"=tab[i]\n";
-            //cout<<c<<"=c\n";
-            tab_de_inten.push_back(c);
-        }
-
-        int test = 0;
-        int i = 0;
-        string st;
-
-        while (i < limit && (trans[i][0] == cel))
-        {
-            st = trans[i].substr(1, trans[i].length() - 2); //ici on prélève la partie de la règle qui nous intéresse
-            cout << st << "=st\n";
-            cout << tab_de_inten << "=tab_de_inten\n";
-            test = st.compare(tab_de_inten);
-            if (test == 0)
-            {
-                size_t j = (trans[i].length() - 1);
-                return trans[i][j];
-            }
-            i++;
-        }
-        return cel;
-    };
-
-    int char_to_int(char c)
-    {
-        int b = 0;
-        char p = c;
-        b = p - '0';
-        return b;
-    }
-
-    void Modele::appliquerTransition(const Configuration &dep, Configuration &dest) const
-    /* Pour chaque cellule de la configuration de départ, récupère ses voisins, récupère son état, et détermine son état d'arrivée qu'elle place dans la confifuration de destinationn*/
-    {
-        if (dep.getReseauLignes() != dest.getReseauLignes() || dep.getReseauColonnes() != dest.getReseauColonnes())
-            dest = dep;
-        char *etatDepart = new char;
-        char etatDest;
-        Etat *e = new Etat;
-        unsigned int p = 0;
-
-        for (int i = 0; i < dep.getReseauLignes(); i++)
-        {
-            for (int j = 0; j < dep.getReseauColonnes(); j++)
-            {
-                while (p < fonctionTrans->taille && ((int)((fonctionTrans->tableau)[p][0]) != ('0' + dep.getEtatCellule(i, j).getIndice())))
+                for (size_t l = 0; l < 10; l++)
                 {
-                    p++;
-                };
-                sprintf(etatDepart, "%d", dep.getEtatCellule(i, j).getIndice());
-                //cout << "voisinnage : "<<dep.getVoisinage(i, j, *typeVoisinnage) << "\n";
-                etatDest = fonctionTrans->comparaison_voisinnage(dep.getVoisinage(i, j, *typeVoisinnage), fonctionTrans->tableau + p, *etatDepart, fonctionTrans->taille - p, etatsPossibles->getNombreEtats());
-                //cout<<"etat dest : "<<char_to_int(etatDest)<<"\n";
-                e = (*etatsPossibles)[char_to_int(etatDest)]; // vis-à-vis de la surcharge de l'opérateur [] à revoir
-                dest.setEtatCellule(i, j, e);
-                p = 0;
+                    if ((i + j + k + l) == nb_voisins)
+                    {
+                        if (j >= 3)
+                        {
+                            tab[m] = "0" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "1";
+                            m++;
+                        }
+                        if (k >= 3)
+                        {
+                            tab[m] = "1" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "2";
+                            m++;
+                        }
+                        if (l >= 3)
+                        {
+                            tab[m] = "2" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "3";
+                            m++;
+                        }
+                        if (i >= 3)
+                        {
+                            tab[m] = "3" + to_string(i) + to_string(j) + to_string(k) + to_string(l) + "0";
+                            m++;
+                        }
+                    }
+                }
             }
         }
     }
-    /*
+    return tab;
+}
+// ça arrive
+char FonctionTransition::comparaison_voisinnage(string voisins, string *trans, char cel, unsigned int limit, int nb_Etat)
+{
+    // pour un voisinnage donné on va vérifier si ce voisinnage est dans
+    // le tableau de fonction transition si oui on va renvoyer un caractère qui
+    // va correspondre à l'état de la cellule à la génération t+1
+    unsigned int i = 0;
+    int test = 0;
+    string st;
+    //cout << "compa_vois"
+    //     << "\n";
+    while (i < limit && (trans[i][0] == cel))
+    {
+        //cout << "passeboucle"
+        //     << "\n\n";
+        st = trans[i].substr(1, trans[i].length() - 2);
+        st.append(st);
+        test = st.find(voisins);
+        if (test != -1)
+        {
+            size_t j = (trans[i].length() - 1);
+            return trans[i][j];
+        }
+        i++;
+    }
+    return cel;
+}
+
+char FonctionTransitionIntention::comparaison_voisinnage(string voisins, string *trans, char cel, unsigned int limit, int nb_Etat)
+{
+    auto tab = new int[nb_Etat]; //initialisation du tableau
+    for (int i = 0; i < nb_Etat; i++)
+    {
+        tab[i] = 0;
+    }
+
+    for (int i = 0; i < voisins.length(); i++)
+    {
+        tab[voisins[i] - '0']++; //pour chaque cellule du voisinnage on va ajouter une occurence à la case du tableau correspond à son état
+    }
+
+    string tab_de_inten;
+    char c;
+    for (int i = 0; i < nb_Etat; i++)
+    { //ici on crée la chaîne de caractère que l'on va comparer avec les règles de transition
+        c = tab[i] + '0';
+        //cout<<tab[i]<<"=tab[i]\n";
+        //cout<<c<<"=c\n";
+        tab_de_inten.push_back(c);
+    }
+
+    int test = 0;
+    int i = 0;
+    string st;
+
+    while (i < limit && (trans[i][0] == cel))
+    {
+        st = trans[i].substr(1, trans[i].length() - 2); //ici on prélève la partie de la règle qui nous intéresse
+        cout << st << "=st\n";
+        cout << tab_de_inten << "=tab_de_inten\n";
+        test = st.compare(tab_de_inten);
+        if (test == 0)
+        {
+            size_t j = (trans[i].length() - 1);
+            return trans[i][j];
+        }
+        i++;
+    }
+    return cel;
+};
+
+int char_to_int(char c)
+{
+    int b = 0;
+    char p = c;
+    b = p - '0';
+    return b;
+}
+
+void Modele::appliquerTransition(const Configuration &dep, Configuration &dest) const
+/* Pour chaque cellule de la configuration de départ, récupère ses voisins, récupère son état, et détermine son état d'arrivée qu'elle place dans la confifuration de destinationn*/
+{
+    if (dep.getReseauLignes() != dest.getReseauLignes() || dep.getReseauColonnes() != dest.getReseauColonnes())
+        dest = dep;
+    char *etatDepart = new char;
+    char etatDest;
+    Etat *e = new Etat;
+    unsigned int p = 0;
+
+    for (int i = 0; i < dep.getReseauLignes(); i++)
+    {
+        for (int j = 0; j < dep.getReseauColonnes(); j++)
+        {
+            while (p < fonctionTrans->taille && ((int)((fonctionTrans->tableau)[p][0]) != ('0' + dep.getEtatCellule(i, j).getIndice())))
+            {
+                p++;
+            };
+            sprintf(etatDepart, "%d", dep.getEtatCellule(i, j).getIndice());
+            //cout << "voisinnage : "<<dep.getVoisinage(i, j, *typeVoisinnage) << "\n";
+            etatDest = fonctionTrans->comparaison_voisinnage(dep.getVoisinage(i, j, *typeVoisinnage), fonctionTrans->tableau + p, *etatDepart, fonctionTrans->taille - p, etatsPossibles->getNombreEtats());
+            //cout<<"etat dest : "<<char_to_int(etatDest)<<"\n";
+            e = (*etatsPossibles)[char_to_int(etatDest)]; // vis-à-vis de la surcharge de l'opérateur [] à revoir
+            dest.setEtatCellule(i, j, e);
+            p = 0;
+        }
+    }
+}
+/*
 Modele creerModele()
 {
     string titre; //key
@@ -288,92 +331,91 @@ Modele creerModele()
 };
 */
 
-    void Modele::sauvegardeM()
+void Modele::sauvegardeM()
+{
+    //Création du doc
+    xml_document doc;
+    string xmlFilePath = "../LO21---Printemps-2021---Projet-Cellulut/Modeles/";
+    xmlFilePath += this->getTitre();
+    xmlFilePath += ".xml";
+
+    //Il faut vérifier qu'aucun modèle avec ce titre n'existe :
+
+    xml_parse_result result = doc.load_file(xmlFilePath.c_str(), parse_default | parse_declaration);
+    if (result)
     {
-        //Création du doc
-        xml_document doc;
-        string xmlFilePath = "../LO21---Printemps-2021---Projet-Cellulut/Modeles/";
-        xmlFilePath += this->getTitre();
-        xmlFilePath += ".xml";
-
-        //Il faut vérifier qu'aucun modèle avec ce titre n'existe :
-
-        xml_parse_result result = doc.load_file(xmlFilePath.c_str(), parse_default | parse_declaration);
-        if (result)
-        {
-            cout << "Il existe deja un modele avec ce titre";
-            return;
-        }
-
-        auto declarationNode = doc.append_child(pugi::node_declaration);
-        //En tête
-        declarationNode.append_attribute("version") = "1.0";
-        //Déclaration balise et attribut
-        xml_node modele = doc.append_child("Modele");
-        xml_node titre = modele.append_child("titre");
-        titre.append_attribute("name") = this->getTitre().c_str();
-
-        xml_node Description = modele.append_child("Description");
-        Description.append_attribute("name") = this->getDescription().c_str();
-
-        xml_node Auteur = modele.append_child("Auteur");
-        Auteur.append_attribute("name") = this->getAuteur().c_str();
-
-        xml_node annee = modele.append_child("AnneeCreation");
-        annee.append_attribute("name") = this->getAnnee();
-
-        //Les attributs d'un état
-        xml_node etats = modele.append_child("Etats");
-        xml_node NbrEtat = etats.append_child("NombreEtat");
-        int nbr = this->getEnsemble()->getNombreEtats();
-        NbrEtat.append_attribute("name") = nbr;
-        //Faire un while sur le nbr d'état
-        Etat *laListe = this->getEnsemble()->getListe();
-        for (int i = 0; i < nbr; i++)
-        {
-            xml_node etat = etats.append_child("Etat");
-            etat.append_attribute("Label") = laListe[i].getLabel().c_str();
-            etat.append_attribute("Couleur") = laListe[i].getCouleur().c_str();
-            etat.append_attribute("Indice") = laListe[i].getIndice();
-        }
-
-        //Gestion du voisinage
-        xml_node voisinage = modele.append_child("Voisinage");
-        xml_node nom = voisinage.append_child("Nom");
-        Voisinage *voi = this->getVoisin();
-        nom.append_attribute("name") = voi->getTypeVoisi().c_str();
-
-        //Quand le voisinage sera terminé
-        /*  xml_node rayon = voisinage.append_child("Rayon");
-    rayon.append_attribute("name")="1";*/
-        //Gestion des éléments, faire un while aussi
-
-        xml_node element = voisinage.append_child("Element");
-        int nbrCase = this->getVoisin()->getNbCelluleVoisi();
-        Case *listeCase = this->getVoisin()->getTableau();
-        for (int i = 0; i < nbrCase; i++)
-        {
-            xml_node caseCoord = element.append_child("Case");
-            caseCoord.append_attribute("X") = listeCase[i].getL();
-            caseCoord.append_attribute("Y") = listeCase[i].getC();
-        }
-
-        //gestion des règles
-
-        xml_node typeF = modele.append_child("TypeFonctionTransition");
-        typeF.append_attribute("name") = getTypeFonction().c_str();
-        xml_node liste = modele.append_child("ListeRegle");
-        const unsigned int tailleR = this->getFonction()->getTaille();
-        string *regles = this->getFonction()->getTableau();
-        for (unsigned int i = 0; i < tailleR; i++)
-        {
-            xml_node regle = liste.append_child("Regle");
-            regle.append_attribute("name") = regles[i].c_str();
-        }
-        xml_node configs = modele.append_child("ListeConfig");
-        configs.append_attribute("nombre") = 0;
-
-        bool saveSuccess = doc.save_file(xmlFilePath.c_str(), PUGIXML_TEXT("   "));
-        cout << saveSuccess;
+        cout << "Il existe deja un modele avec ce titre";
+        return;
     }
 
+    auto declarationNode = doc.append_child(pugi::node_declaration);
+    //En tête
+    declarationNode.append_attribute("version") = "1.0";
+    //Déclaration balise et attribut
+    xml_node modele = doc.append_child("Modele");
+    xml_node titre = modele.append_child("titre");
+    titre.append_attribute("name") = this->getTitre().c_str();
+
+    xml_node Description = modele.append_child("Description");
+    Description.append_attribute("name") = this->getDescription().c_str();
+
+    xml_node Auteur = modele.append_child("Auteur");
+    Auteur.append_attribute("name") = this->getAuteur().c_str();
+
+    xml_node annee = modele.append_child("AnneeCreation");
+    annee.append_attribute("name") = this->getAnnee();
+
+    //Les attributs d'un état
+    xml_node etats = modele.append_child("Etats");
+    xml_node NbrEtat = etats.append_child("NombreEtat");
+    int nbr = this->getEnsemble()->getNombreEtats();
+    NbrEtat.append_attribute("name") = nbr;
+    //Faire un while sur le nbr d'état
+    Etat *laListe = this->getEnsemble()->getListe();
+    for (int i = 0; i < nbr; i++)
+    {
+        xml_node etat = etats.append_child("Etat");
+        etat.append_attribute("Label") = laListe[i].getLabel().c_str();
+        etat.append_attribute("Couleur") = laListe[i].getCouleur().c_str();
+        etat.append_attribute("Indice") = laListe[i].getIndice();
+    }
+
+    //Gestion du voisinage
+    xml_node voisinage = modele.append_child("Voisinage");
+    xml_node nom = voisinage.append_child("Nom");
+    Voisinage *voi = this->getVoisin();
+    nom.append_attribute("name") = voi->getTypeVoisi().c_str();
+
+    //Quand le voisinage sera terminé
+    /*  xml_node rayon = voisinage.append_child("Rayon");
+    rayon.append_attribute("name")="1";*/
+    //Gestion des éléments, faire un while aussi
+
+    xml_node element = voisinage.append_child("Element");
+    int nbrCase = this->getVoisin()->getNbCelluleVoisi();
+    Case *listeCase = this->getVoisin()->getTableau();
+    for (int i = 0; i < nbrCase; i++)
+    {
+        xml_node caseCoord = element.append_child("Case");
+        caseCoord.append_attribute("X") = listeCase[i].getL();
+        caseCoord.append_attribute("Y") = listeCase[i].getC();
+    }
+
+    //gestion des règles
+
+    xml_node typeF = modele.append_child("TypeFonctionTransition");
+    typeF.append_attribute("name") = getTypeFonction().c_str();
+    xml_node liste = modele.append_child("ListeRegle");
+    const unsigned int tailleR = this->getFonction()->getTaille();
+    string *regles = this->getFonction()->getTableau();
+    for (unsigned int i = 0; i < tailleR; i++)
+    {
+        xml_node regle = liste.append_child("Regle");
+        regle.append_attribute("name") = regles[i].c_str();
+    }
+    xml_node configs = modele.append_child("ListeConfig");
+    configs.append_attribute("nombre") = 0;
+
+    bool saveSuccess = doc.save_file(xmlFilePath.c_str(), PUGIXML_TEXT("   "));
+    cout << saveSuccess;
+}
