aabefc38d52eae4c609a8e5cea47e0aadc88ca7f
toto5246 <thom.guegan@gmail.com>
2021-06-13 16:51:26 +0200

    Doxygen voisinage

--- voisinage.cpp
+++ voisinage.cpp
@@ -1,11 +1,25 @@
-//
-// Created by thoma on 13/05/2021.
-//
+/**
+ * @file voisinage.cpp
+ * @author Thomas Guegan / Violette Durocher (you@domain.com)
+ * @brief fonctions de la classe Voisinage
+ * @version 0.1
+ * @date 2021-06-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 
 #include "voisinage.h"
 #include <vector>
 #include <algorithm>
 
+
+
+/**
+ * @brief Fonction permettant de définir l'ensemble de case
+ *
+ * @param[in] tableau de cases
+ */
 void Voisinage::setensemble_case(std::vector<Case> tableau)
 {
     nbCelluleVoisi = tableau.size();
@@ -30,6 +44,12 @@ void Voisinage::setensemble_case(Case* tableau)
     this->ensemble_case = tableau;
 }
 
+
+/**
+ * @brief définition du voisinage de VonNeuman
+ *
+ * @param[in] rayon
+ */
 void V_VonNeumann::definir_ensemble_case(int rayon)
 {
 
@@ -84,11 +104,15 @@ void V_VonNeumann::definir_ensemble_case(int rayon)
     tableau.clear();
 }
 
-
+/**
+ * @brief définition du voisinage de Moore
+ *
+ * @param[in] rayon
+ */
 void V_Moore::definir_ensemble_case(int rayon)
 {
     nbCelluleVoisi = (2*rayon +1)*(2*rayon +1) - 1;
-    //cout << "*** " << nbCelluleVoisi << " cases reservees ***" << endl; // ok
+
 
     ensemble_case = new Case[nbCelluleVoisi]; // on réserve la place pour le bon nombre de cellules
 
@@ -96,63 +120,69 @@ void V_Moore::definir_ensemble_case(int rayon)
 
     for (int r=1; r<= rayon; r++)
     {
-        //cout << "\tcouronne " << r << endl;
 
-        //cout << " ligne du haut (coins inclus) " << endl;
         for (int j=-r; j<=r; j++)
         {
-            //cout << ind;
+
             ensemble_case[ind].setL_C(r,j);
-            //cout << "[" << ensemble_case[ind].getL() << "," << ensemble_case[ind].getC() << "] ";
+
             ind++;
         }
-        //cout << endl;
 
-        //cout << "ligne de droite (coins exclus)" << endl;
         for (int i=r-1; i>=-r+1; i--)
         {
-            //cout << ind;
+
             ensemble_case[ind].setL_C(i,r);
-            //cout << "[" << ensemble_case[ind].getL() << "," << ensemble_case[ind].getC() << "] ";
+
             ind++;
         }
-        //cout << endl;
 
-        //cout << "ligne du bas (coins inclus)" << endl;
         for (int j=r; j>=-r; j--)
         {
-            //cout << ind;
+
             ensemble_case[ind].setL_C(-r,j);
-            //cout << "[" << ensemble_case[ind].getL() << "," << ensemble_case[ind].getC() << "] ";
+
             ind++;
         }
-        //cout << endl;
 
-        //cout << "ligne de gauche (coins exclus)" << endl;
         for (int i=-r+1; i<=r-1; i++)
         {
-            //cout << ind;
+
             ensemble_case[ind].setL_C(i,-r);
-            //cout << "[" << ensemble_case[ind].getL() << "," << ensemble_case[ind].getC() << "] ";
+
             ind++;
         }
-        //cout << endl;
+
     }
 
 }
 
+/**
+ * @brief getter Nombre de cellule
+ *
+ * @return nombre de cellule
+ */
 size_t Voisinage::getNbCelluleVoisi() const
 {
     return this->nbCelluleVoisi;
 }
 
+/**
+ * @brief getter Type Voisinage
+ *
+ * @return type du voisinage
+ */
 const string Voisinage::getTypeVoisi()
 {
     return this->typeVoisi;
 }
 
 
-//Ne pas commenter j'en ai besoin svp ;)
+/**
+ * @brief getter Tableau du voisinage
+ *
+ * @return tableau du voisinage
+ */
 Case *Voisinage::getTableau()
 {
     return this->ensemble_case;
