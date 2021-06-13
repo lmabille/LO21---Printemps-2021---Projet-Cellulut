6af75571dbc3ca64adf8c087691229da8dc7a921
toto5246 <thom.guegan@gmail.com>
2021-06-13 19:46:24 +0200

    doxy

--- etat_reseau.cpp
+++ etat_reseau.cpp
@@ -1,9 +1,27 @@
 //
 // Created by Anne LAHNECHE on 05/05/2021.
 //
+/**
+ * @file etat_reseau.cpp
+ * @author Orhane
+ * @brief
+ * @version 0.1
+ * @date 2021-06-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+
 
 #include "etat_reseau.h"
 
+
+/**
+ * @brief setter d'etat de cellule
+ *
+ * @param[in] nouvel etat
+ */
 void Cellule::set_etatcellule(Etat * e){
     etat=e;
 }
